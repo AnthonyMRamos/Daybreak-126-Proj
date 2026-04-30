9a544a41721328bd2dc1c8e5cf86ad157d08c913
MathanRamos <mathanramos11@gmail.com>
2026-04-29 18:13:52 -0700

    Revert "deduplicate new table"
    This reverts commit b44509379acccfa3da8ac46a0947ad7bca682d47.

--- Duplicate_Removal.sql
+++ Duplicate_Removal.sql
@@ -1,10 +1,17 @@
-DELETE FROM combinedHospitalInfo
+DELETE FROM combined_2010_to_2023_hospital_data
 WHERE rowid NOT IN (
     SELECT MIN(rowid)
-    FROM combinedHospitalInfo
+    FROM combined_2010_to_2023_hospital_data
     GROUP BY
+        YEAR,
+        COUNTY,
         HOSPITAL,
         OSHPDID,
+        "Procedure/Condition",
+        "Risk Adjusted Mortality Rate",
+        "# of Deaths",
+        "# of Cases",
+        "Hospital Ratings",
         LONGITUDE,
         LATITUDE
 );
